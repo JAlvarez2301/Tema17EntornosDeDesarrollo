308e72a67a74a5f38b87fe471aab9c57e525b6c5
JAlvarez2301 <124701952+JAlvarez2301@users.noreply.github.com>
2023-04-18 15:22:49 +0200

    Update optimizar.py

--- optimizar.py
+++ optimizar.py
@@ -1,8 +1,9 @@
 class optimizar:
 
+    
     array = []
 for i in range(3):
-    n = int(input("Introduzca un número: "))
-    array.append(n)
-media = sum(array) / len(array)
-print("La media es " + str(media))
+    n = int(input("Introduzca un número: ")) #Pedimos al usuario que de un valor a la variable
+    array.append(n) #Añade un elemento al final del array
+media = sum(array) / len(array) #Media va a ser igual a: la suma de los valores del array dividido entre la longitud del array
+print("La media es " + str(media)) #Importante hacer el casting con str para que no de error el hacer el print
